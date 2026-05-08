b1869f74061ad5db28f5f45e08f51dc2e041bf4d
g02t <timmattias.juksaar@gmail.com>
2026-05-08 22:45:19 +0300

    Voidu stseen

--- src/main/java/ryhmatoo/oopryhmatoo2/Edetabel.java
+++ src/main/java/ryhmatoo/oopryhmatoo2/Edetabel.java
@@ -1,14 +1,26 @@
 package ryhmatoo.oopryhmatoo2;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class Edetabel implements StseeniLooja{
 
     public Scene looStseen() {
         Pane juur = new Pane();
 
+        Image taust = new Image("Edetabel_taust.png");
+        BackgroundImage bg = new BackgroundImage(taust,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize(100, 100,true, true, false, false));
 
+        juur.setBackground(new Background(bg));
+        Scene stseen = new Scene(juur, 1200, 720);
+
+        return new Scene(juur);
     }
 
 }
